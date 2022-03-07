043e2d35476fa13e5236e4d93ac27a6ccb785b39
Aron Adams <aronrapetto@hotmail.com>
2022-03-07 16:05:47 -0300

    exercicios concluido

--- Fundamentos/Bloco_4_introcucao_a_javascript_e_logica_de_programacao/exercicios_fixao/tipos_primitivos_tipagem_dinamica_e_operacoes_aritmeticas/scripts.js
+++ Fundamentos/Bloco_4_introcucao_a_javascript_e_logica_de_programacao/exercicios_fixao/tipos_primitivos_tipagem_dinamica_e_operacoes_aritmeticas/scripts.js
@@ -1,7 +1,32 @@
-let patientId = 50;
-let isEnrolled = true;
-const patientInfo = {
-  firstName: 'Ana',
-  lastName: 'Santos',
-};
-const patientEmail = 'ana@email.com';
+// Exemplo de aula (comentado)
+
+// let patientId = '50';
+// let isEnrolled = true;
+// const patientInfo = {
+//   firstName: 'Ana',
+//   lastName: 'Santos',
+// };
+// const patientEmail = 'ana@email.com';
+
+// console.log(typeof patientId);
+// console.log(typeof isEnrolled);
+// console.log(typeof patientInfo);
+// console.log(typeof patientEmail);
+
+// console.log(typeof patientAge);
+
+// Exercicio
+
+const base = 5;
+let height = 8;
+
+const area = base * height;
+
+console.log(area);
+
+const perimeter = (base*2)+(height*2);
+
+console.log(perimeter);
+
+
+
