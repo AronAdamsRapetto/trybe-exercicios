578ba5aadf9d4d3af1deac707262c945b1be2961
Aron Adams <aronrapetto@hotmail.com>
2022-03-10 00:11:54 -0300

    concluida segunda parte do exercicio 3

--- Fundamentos/Bloco_4_introcucao_a_javascript_e_logica_de_programacao/dia-3-logica-de-programacao-e-algoritmos/exercicio-aula/exercicio-3.js
+++ Fundamentos/Bloco_4_introcucao_a_javascript_e_logica_de_programacao/dia-3-logica-de-programacao-e-algoritmos/exercicio-aula/exercicio-3.js
@@ -1,11 +1,19 @@
 let array = ['java', 'javascript', 'python', 'html', 'css'];
 let maiorPalavra = "";
+let menorPalavra;
 
 for(let index = 0; index < array.length; index += 1){
+    if(menorPalavra === undefined){
+        menorPalavra = array[index];
+    }
     for(let index2 = 0; index2 < array.length; index2 += 1){
-        if(array[index].length > array[index2].length && array[index].length > maiorPalavra.length ){
+        if(array[index].length > array[index2].length && array[index].length > maiorPalavra.length){
             maiorPalavra = array[index];
+        }
+        else if(array[index].length < array[index2].length && array[index].length < menorPalavra.length){
+            menorPalavra = array[index];
         }
     }
 }
-console.log(maiorPalavra);
+console.log("maior palavra: " + maiorPalavra);
+console.log("menor paravra: " + menorPalavra);
