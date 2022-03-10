7e389febb04ca115e96cc21c9f3a26910197dc01
Aron Adams <aronrapetto@hotmail.com>
2022-03-10 15:32:45 -0300

    exercicio bonus 5 concluido

---
+++ Fundamentos/Bloco_4_introcucao_a_javascript_e_logica_de_programacao/dia-3-logica-de-programacao-e-algoritmos/exercicio-aula/exercicios-bonus/exercicio-5.js
@@ -0,0 +1,32 @@
+const n = 7;
+let auxLinhas = 0;
+let auxEspacos = (n / 2);
+auxEspacos = Math.floor(auxEspacos) + 1;
+
+if(n % 2 != 0){
+    for(let linha = 0; linha <= n; linha += 1){
+        auxLinhas += 1;
+        let geoPiramide = "";
+        if(auxLinhas % 2 != 0){
+            auxEspacos -= 1;    
+            for(let coluna = 1; coluna <= n; coluna += 1){
+                if(linha === n-1){
+                    geoPiramide = geoPiramide + "*";
+                }       
+                else if(coluna < auxEspacos || coluna > n - auxEspacos){
+                    geoPiramide = geoPiramide + " ";
+                }                
+                else if(coluna === auxEspacos + 1 && auxLinhas != 1 || coluna === n - auxEspacos){
+                    geoPiramide = geoPiramide + "*";
+                }
+                else{
+                    geoPiramide = geoPiramide + " ";
+                }
+            }
+        }
+        console.log(geoPiramide);    
+    }
+}
+else{
+    console.log("numero não é par...");
+}
