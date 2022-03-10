a78e8b769aafc02d6d82f24e50b8d4bd0b64ec82
Aron Adams <aronrapetto@hotmail.com>
2022-03-10 10:07:37 -0300

    exercicio bonus 1 refatorado

--- Fundamentos/Bloco_4_introcucao_a_javascript_e_logica_de_programacao/dia-3-logica-de-programacao-e-algoritmos/exercicio-aula/exercicios-bonus/exercicio-1.js
+++ Fundamentos/Bloco_4_introcucao_a_javascript_e_logica_de_programacao/dia-3-logica-de-programacao-e-algoritmos/exercicio-aula/exercicios-bonus/exercicio-1.js
@@ -1,9 +1,11 @@
-const n = 5;
+const n = 2;
 
-for(let linha = 0; linha < n; linha += 1){
+if(n > 1){
+  for(let linha = 0; linha < n; linha += 1){
   let geoQuadrado = "";
   for(let coluna = 0; coluna < n; coluna += 1){
     geoQuadrado = geoQuadrado + "*";
   }
   console.log(geoQuadrado);
+  }
 }
