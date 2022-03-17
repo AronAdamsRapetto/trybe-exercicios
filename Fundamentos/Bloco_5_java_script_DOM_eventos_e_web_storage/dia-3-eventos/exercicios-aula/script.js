21906cefebc298e60bf53c24621ceb74617369f7
Aron Adams <aronrapetto@hotmail.com>
2022-03-17 00:03:54 -0300

    exercicio aula concluido

--- Fundamentos/Bloco_5_java_script_DOM_eventos_e_web_storage/dia-3-eventos/exercicios-aula/script.js
+++ Fundamentos/Bloco_5_java_script_DOM_eventos_e_web_storage/dia-3-eventos/exercicios-aula/script.js
@@ -13,7 +13,7 @@ function createDaysOfTheWeek() {
   
   createDaysOfTheWeek();
   
-  //ex 1
+  // ex 1
   const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
   let paiLiDias = document.getElementById('days');
   let dia = [];
@@ -67,7 +67,7 @@ function createDaysOfTheWeek() {
     }
   }
 
-  //ex 5
+  // ex 5
 
   let armazenaDiasSexta = [];
   function alteraSexta(){
@@ -82,7 +82,7 @@ function createDaysOfTheWeek() {
     }
   }
 
-  //ex 6
+  // ex 6
 
   function destacaDia(event){
     event.target.style.fontSize = '2em';
@@ -92,13 +92,76 @@ function createDaysOfTheWeek() {
     event.target.style.fontSize = '20px';
   }
 
-  //ex 7
+  // ex 7
 
   function addTask(task){
     let paiDeSpan = document.getElementsByClassName('my-tasks');
     let criaSpan = document.createElement('span');
     criaSpan.innerText = task;
     paiDeSpan[0].appendChild(criaSpan);
+  }
+
+  // ex 8
+
+  function addLegendaTask(cor){
+    let paiDeDiv = document.getElementsByClassName('my-tasks');
+    let criaDiv = document.createElement('div');
+    criaDiv.classList = 'tasks';
+    criaDiv.style.backgroundColor = cor;
+    paiDeDiv[0].appendChild(criaDiv);
+  }
+
+  // ex 9
+
+  function selecionaTask(event){
+    if(event.target.classList == 'tasks'){ // duvida para mentoria
+      event.target.classList.add('task-selected');
+    } else {
+      event.target.classList.remove('task-selected');
+    }
+  }
+
+  // ex 10
+
+  function marcaDia(event){
+    let taskSelecionada = document.getElementsByClassName('task-selected');
+    if(event.target.style.color !== taskSelecionada[0].style.backgroundColor){
+      event.target.style.color = taskSelecionada[0].style.backgroundColor;
+    } else {
+      event.target.style.color = 'rgb(119,119,119)';
+    }
+  }
+
+  // ex bonus
+
+    function criaCompromisso(event){
+      let texto = document.getElementById('task-input');          
+      let criaCompromisso = document.createElement('li');      
+      criaCompromisso.innerText = texto.value;
+      
+      return criaCompromisso;
+    }
+
+    function addCompromisso(){
+    let texto = document.getElementById('task-input');
+    let listaDeCompromissos = document.getElementsByClassName('task-list');    
+    let compromisso = criaCompromisso(); 
+    
+    if (texto.value === ''){
+      alert('Erro ao clicar em adicionar, algo deve ser digitado!');
+    } else {
+      listaDeCompromissos[0].appendChild(compromisso);
+    }
+  }
+
+  function addCompromissoEnter(event){    
+    let listaDeCompromissos = document.getElementsByClassName('task-list');
+    let compromisso = criaCompromisso();
+    let tecla = event.key;
+
+    if(tecla === 'Enter'){
+      listaDeCompromissos[0].appendChild(compromisso);  // duvida referente a event.key
+    }
   }
   
   // chamada das funcoes
@@ -109,16 +172,16 @@ function createDaysOfTheWeek() {
   addClasseFeriados();
   addClasseSexta();
   
-  //ex 2
+  // ex 2
 
   criaBotao('Feriados', 'btn-holiday'); 
   
-  //ex 3
+  // ex 3
   
   let botaoFeriado = document.getElementById('btn-holiday');
   botaoFeriado.addEventListener('click', destacaFeriados);
 
-  //ex 4
+  // ex 4
 
   criaBotao('Sexta-feira', 'btn-friday');
 
@@ -127,7 +190,7 @@ function createDaysOfTheWeek() {
   let botaoSexta = document.getElementById('btn-friday');
   botaoSexta.addEventListener('click', alteraSexta);  
 
-  //ex 6
+  // ex 6
 
   let dias = document.getElementsByClassName('day');
   for(let index = 0; index < dias.length; index += 1){
@@ -135,5 +198,31 @@ function createDaysOfTheWeek() {
     dias[index].addEventListener('mouseleave', normalizaDia);
   }
 
+  // ex 7
+
   addTask('Terminar exercícios da trybe');
+
+  // ex 8
+
+  addLegendaTask('green');
+
+  // ex 9
+
+  let task = document.getElementsByClassName('tasks');
+  for(let index = 0; index < task.length; index += 1){
+    task[index].addEventListener('click', selecionaTask);
+  }
+
+  // ex 10
+
+  for(let index = 0; index < dias.length; index += 1){
+    dias[index].addEventListener('click', marcaDia);
+  }
+  
+  // ex bonus
+  let botoaoAddCompromisso = document.getElementById('btn-add');
+  botoaoAddCompromisso.addEventListener('click', addCompromisso);
+
+  let caixaTextoCompromisso = document.getElementById('task-input');
+  caixaTextoCompromisso.addEventListener('keypress', addCompromissoEnter);
 
