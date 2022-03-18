62d243f072123c763c8d9925cd72013390208edf
Aron Adams <aronrapetto@hotmail.com>
2022-03-18 17:07:47 -0300

    resolucao para remocao de paragrafo especifico pendente

--- Fundamentos/Bloco_5_java_script_DOM_eventos_e_web_storage/dia-4-web-storage/exercicios-aula/script.js
+++ Fundamentos/Bloco_5_java_script_DOM_eventos_e_web_storage/dia-4-web-storage/exercicios-aula/script.js
@@ -4,15 +4,19 @@ let botaoMudaCorTexto = document.getElementById('btn-corTexto');
 let botaoMudaFontSize = document.getElementById('btn-tamanhoFonte');
 let botaoMudaEspacLinhas = document.getElementById('btn-espacoLinhas');
 let botaoMudaFonte = document.getElementById('btn-Fonte');
+let botaoAddParagrado = document.getElementById('btn-Addparagrafo');
+let botaoDelParagrafo = document.getElementById('btn-Delparagrafo');
+let botaoDelAll = document.getElementById('btn-DelAllParafrafo');
 let alteracoes = {
     corFundo: '',
     corTexto: '',
     tamanhoFonte: 0,
     espacoLinhas: 0,
-    fonte: ''
+    fonte: ''    
 };
+let paragrafoList = [];
 
-function armazenaValor (objeto){
+function armazenaEstilo (objeto){
     localStorage.setItem('alteracoes', JSON.stringify(objeto));
 }
 
@@ -20,35 +24,104 @@ function addCorFundo (){
     let corPedida = document.getElementById('input-corFundo');
     boxParagrafo.style.backgroundColor = corPedida.value;
     alteracoes.corFundo = corPedida.value;    
-    armazenaValor(alteracoes);
+    armazenaEstilo(alteracoes);
 }
 
 function alteraCorTexto(){
     let corPedida = document.getElementById('input-corTexto');
     boxParagrafo.style.color = corPedida.value;
     alteracoes.corTexto = corPedida.value;
-    armazenaValor(alteracoes);
+    armazenaEstilo(alteracoes);
 }
 
 function alteraTamanhoFonte(){
     let tamanho = document.getElementById('input-tamanhoFonte');
     boxParagrafo.style.fontSize = tamanho.value + 'px';
     alteracoes.tamanhoFonte = tamanho.value;
-    armazenaValor(alteracoes);    
+    armazenaEstilo(alteracoes);    
 }
 
 function alteraEspacLinhas(){
     let espaco = document.getElementById('input-espacoLinhas');
     boxParagrafo.style.lineHeight = espaco.value + 'px';
     alteracoes.espacoLinhas = espaco.value;
-    armazenaValor(alteracoes);
+    armazenaEstilo(alteracoes);
 }
 
 function alteraFonte(){
     let fonte = document.getElementById('input-Fonte');
     boxParagrafo.style.fontFamily = fonte.value;
     alteracoes.fonte = fonte.value;
-    armazenaValor(alteracoes);
+    armazenaEstilo(alteracoes);
+}
+
+// Adicionais
+
+function armazenaParagrafo(){
+    localStorage.setItem('paragrafos', JSON.stringify(paragrafoList));    
+}
+
+function selecionaParagrafo(event){
+    if(event.target.classList != 'paragrafoAdd'){
+        event.target.classList.remove('selected');
+    } else {        
+        event.target.classList.add('selected');
+    }
+}
+
+function criaParagrafo(){
+    let texto = document.getElementById('paragrafo');   
+    let paragrafo = document.createElement('p');    
+    paragrafo.innerText = texto.value;
+    paragrafo.classList = 'paragrafoAdd';
+    paragrafoList.push(texto.value);
+    texto.value = '';    
+    return paragrafo; 
+}
+
+function addParagrafo(){
+    let titulo = document.getElementsByTagName('h2');
+    let paragrafo = criaParagrafo();
+    paragrafo.addEventListener('click', selecionaParagrafo);
+    boxParagrafo.appendChild(paragrafo);
+    armazenaParagrafo();
+    titulo[0].innerText = '';                
+}
+
+function delParagrafo(){
+    let paragrafoSelecionado = document.getElementsByClassName('paragrafoAdd');
+    paragrafoList = [];    
+    for(let index = 0; index < paragrafoSelecionado.length; index += 1){
+        if(paragrafoSelecionado[index].classList != 'paragrafoAdd'){
+            paragrafoSelecionado[index].remove();            
+        } else {
+            paragrafoList.push(paragrafoSelecionado[index].innerText);
+        }
+    }
+    localStorage.setItem('paragrafos', JSON.stringify(paragrafoList));
+}
+let paragrafoSelecionado = document.getElementsByClassName('paragrafoAdd');
+
+function delAll(){
+    boxParagrafo.innerHTML = '<h2>Textos serão exibidos aqui</h2>';
+    localStorage.clear();        
+}
+
+function renderizaParagrafos(){
+    if(localStorage.getItem('paragrafos') === null){
+        localStorage.setItem('paragrafos', JSON.stringify([]));
+    } else {
+        paragrafoList = JSON.parse(localStorage.getItem('paragrafos'));
+        let titulo = document.getElementsByTagName('h2');        
+        for(let index = 0; index < paragrafoList.length; index += 1){
+            let novoParagrafo = document.createElement('p');
+            novoParagrafo.innerText = paragrafoList[index];
+            novoParagrafo.classList = 'paragrafoAdd';
+            novoParagrafo.addEventListener('click', selecionaParagrafo);
+            boxParagrafo.appendChild(novoParagrafo);
+            titulo[0].innerText = '';
+        }
+    }
 }
 
 botaoMudaCorDeFundo.addEventListener('click', addCorFundo);
@@ -57,12 +130,22 @@ botaoMudaFontSize.addEventListener('click', alteraTamanhoFonte);
 botaoMudaEspacLinhas.addEventListener('click', alteraEspacLinhas);
 botaoMudaFonte.addEventListener('click', alteraFonte);
 
-
-window.onload = function(){    
+window.onload = function(){ 
+    if(localStorage.getItem('alteracoes') === null){
+        localStorage.setItem('alteracoes', JSON.stringify({}));
+    } else {   
     alteracoes = JSON.parse(localStorage.getItem('alteracoes'));
     boxParagrafo.style.backgroundColor = alteracoes.corFundo;
     boxParagrafo.style.color = alteracoes.corTexto;
     boxParagrafo.style.fontSize = alteracoes.tamanhoFonte + 'px';
     boxParagrafo.style.lineHeight = alteracoes.espacoLinhas + 'px';
-    boxParagrafo.style.fontFamily = alteracoes.fonte;       
+    boxParagrafo.style.fontFamily = alteracoes.fonte;
+    renderizaParagrafos(); 
+    }      
 }
+
+// adicionais
+
+botaoAddParagrado.addEventListener('click', addParagrafo);
+botaoDelParagrafo.addEventListener('click', delParagrafo);
+botaoDelAll.addEventListener('click', delAll);
