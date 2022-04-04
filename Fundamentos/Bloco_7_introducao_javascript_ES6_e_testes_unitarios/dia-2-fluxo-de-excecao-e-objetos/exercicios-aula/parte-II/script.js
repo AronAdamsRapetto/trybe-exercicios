2e1016f2e707ea2a9a2ee3c77dbc4e4521bb2b40
Aron Adams <aronrapetto@hotmail.com>
2022-04-04 19:35:58 -0300

    exercicios aula parte II concluidos

--- Fundamentos/Bloco_7_introducao_javascript_ES6_e_testes_unitarios/dia-2-fluxo-de-excecao-e-objetos/exercicios-aula/parte-II/script.js
+++ Fundamentos/Bloco_7_introducao_javascript_ES6_e_testes_unitarios/dia-2-fluxo-de-excecao-e-objetos/exercicios-aula/parte-II/script.js
@@ -36,14 +36,18 @@ const order = {
   
   const customerInfo = (order) => {
     // Adicione abaixo as informações necessárias.
-  
+    return `Olá ${order.order.delivery.deliveryPerson}, entrega para: ${order.name}, telefone: ${order.phoneNumber}, R. ${order.address.street}, N°: ${order.address.number}, AP: ${order.address.apartment}`;  
   }
   
-  customerInfo(order);
+  console.log(customerInfo(order));
   
   const orderModifier = (order) => {
     // Adicione abaixo as informações necessárias.
+    order.name = 'Luiz Silva';
+    order.payment.total = 50;
+    const pizzas = Object.keys(order.order.pizza);
+    return `Olá, ${order.name}, o total do seu pedido de ${pizzas[0]}, ${pizzas[1]} e ${order.order.drinks.coke.type}, é R$${order.payment.total},00.`
   
   }
-  
-  orderModifier(order);
+
+  console.log(orderModifier(order));
