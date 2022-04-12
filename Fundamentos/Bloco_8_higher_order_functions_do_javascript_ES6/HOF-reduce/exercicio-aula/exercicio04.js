cdb8d82c3723086f01790cd2b61e44d7e27a8819
Aron Adams <aronrapetto@hotmail.com>
2022-04-12 19:05:31 -0300

    exercicio 4 concluido

--- Fundamentos/Bloco_8_higher_order_functions_do_javascript_ES6/HOF-reduce/exercicio-aula/exercicio04.js
+++ Fundamentos/Bloco_8_higher_order_functions_do_javascript_ES6/HOF-reduce/exercicio-aula/exercicio04.js
@@ -62,3 +62,23 @@ const books = [
   ];
   
   // Adicione o código do exercício aqui:
+
+const expectedResult = {
+    id: 1,
+    name: 'As Crônicas de Gelo e Fogo',
+    genre: 'Fantasia',
+    author: {
+      name: 'George R. R. Martin',
+      birthYear: 1948,
+    },
+    releaseYear: 1991,
+  };
+  
+  function longestNamedBook() {
+    // escreva seu código aqui
+    const maiorNome = books.reduce((maiorNome, livro) => (maiorNome < livro) ? livro : maiorNome, '');
+
+    return maiorNome;
+  };
+
+  console.log(longestNamedBook());
