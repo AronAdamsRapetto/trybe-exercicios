d660e40ad1391142f5457e9ab3ae018b9ae3c2f5
Aron Adams <aronrapetto@hotmail.com>
2022-04-13 20:04:14 -0300

    exercicios fixacao concluidos

---
+++ Fundamentos/Bloco_8_higher_order_functions_do_javascript_ES6/dia-5-higher-order-function-spreadOperator-parametroRest-destructuring-e-mais/exercicios-fixacao/spread.js
@@ -0,0 +1,12 @@
+// Faça uma lista com as suas frutas favoritas
+const specialFruit = ['Banana', 'Maça', 'Morango'];
+
+// Faça uma lista de complementos que você gostaria de adicionar
+const additionalItens = ['Canela', 'Chocolate', 'Granulado'];
+
+const fruitSalad = (fruit, additional) => {
+  // Esreva sua função aqui
+  return novoArray = [...fruit, ...additional]
+};
+
+console.log(fruitSalad(specialFruit, additionalItens));
