0935bd23c30df5b6e135ce4dae3f6ff663e752fd
Aron Adams <aronrapetto@hotmail.com>
2022-05-11 20:34:20 -0300

    ...

--- Front-end/Bloco_11_Componentes_com_estado_Eventos_e_Formularios_com_React/dia-1-componentes-com-estado-e-eventos/exercicios-fixacao/exercicio-1/src/App.js
+++ Front-end/Bloco_11_Componentes_com_estado_Eventos_e_Formularios_com_React/dia-1-componentes-com-estado-e-eventos/exercicios-fixacao/exercicio-1/src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor() {
+    super()
+
+    this.state = {
+      clickPrimeiroBotao: 0,
+      eParPrimeiroBotao: false,
+      clickSegundoBotao: 0,
+      eParSegundoBotao: false,
+      clickTerceiroBotao: 0,
+      eParTerceiroBotao: false,
+    }
+
+    this.primeiroClick = this.primeiroClick.bind(this);
+    this.segundoClick = this.segundoClick.bind(this);
+    this.terceiroClick = this.terceiroClick.bind(this);
+  }
+  
+  primeiroClick(event) {
+    this.setState((estadoAnterior, _props) => ({
+      clickPrimeiroBotao : estadoAnterior.clickPrimeiroBotao + 1,
+    }));
+    // if (this.state.clickPrimeiroBotao % 2 === 0) {
+    //   this.setState({
+    //     eParPrimeiroBotao: false,
+    //   })
+    // } else {
+    //   this.setState({
+    //     eParPrimeiroBotao: true,
+    //   })
+    // }
+    if(this.state.clickPrimeiroBotao % 2 === 0) {
+      event.target.style.color = 'black';
+    } else {
+      event.target.style.color = 'green';
+    }
+  }
+  
+  segundoClick() {
+    this.setState((estadoAnterior, _props) => ({
+      clickSegundoBotao : estadoAnterior.clickSegundoBotao + 1,
+    }));
+    if (this.state.clickSegundoBotao % 2 === 0) {
+      this.setState({
+        eParSegundoBotao: false,
+      })
+    } else {
+      this.setState({
+        eParSegundoBotao: true,
+      })
+    }
+  }
+  
+  terceiroClick() {
+    this.setState((estadoAnterior, _props) => ({
+      clickTerceiroBotao : estadoAnterior.clickTerceiroBotao + 1,
+    }));
+    if (this.state.clickTerceiroBotao % 2 === 0) {
+      this.setState({
+        eParTerceiroBotao: false,
+      })
+    } else {
+      this.setState({
+        eParTerceiroBotao: true,
+      })
+    }
+  }
+
+    render() {      
+      return (
+        <div className="App">
+          <button onClick={ this.primeiroClick } className={this.state.eParPrimeiroBotao ? "corBotao" : ""}>
+            Primeiro botão {this.state.clickPrimeiroBotao}
+            </button>
+          <button onClick={ this.segundoClick } className={this.state.eParSegundoBotao ? "corBotao" : ""}>
+            Segundo botão {this.state.clickSegundoBotao}
+            </button>
+          <button onClick={ this.terceiroClick } className={this.state.eParTerceiroBotao ? "corBotao" : ""}>
+            Terceiro botão {this.state.clickTerceiroBotao}
+            </button>
+        </div>
+    );
+  }
 }
+
 
 export default App;
