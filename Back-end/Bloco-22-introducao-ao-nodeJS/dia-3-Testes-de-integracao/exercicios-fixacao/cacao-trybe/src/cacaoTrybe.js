02478662566d3f528ba00f618b5e34ad14d22bcb
Aron Adams <aronrapetto@hotmail.com>
2022-08-23 21:56:10 -0300

    execicios aula concluidos

--- Back-end/Bloco-22-introducao-ao-nodeJS/dia-3-Testes-de-integracao/exercicios-fixacao/cacao-trybe/src/cacaoTrybe.js
+++ Back-end/Bloco-22-introducao-ao-nodeJS/dia-3-Testes-de-integracao/exercicios-fixacao/cacao-trybe/src/cacaoTrybe.js
@@ -11,6 +11,16 @@ const readCacaoTrybeFile = async () => {
   }
 };
 
+const writeCacaoTrybeFile = async (newContent) => {
+  const path = '/files/cacaoTrybeFile.json';
+  try {
+    await fs.writeFile(join(__dirname, path), JSON.stringify(newContent));
+  } catch (error) {
+    console.log('Erro ao alterar arquivo: ', error.message);
+    return null;
+  }
+}
+
 const getAllChocolates = async () => {
   const cacaoTrybe = await readCacaoTrybeFile();
   return cacaoTrybe.chocolates;
@@ -28,8 +38,36 @@ const getChocolatesByBrand = async (brandId) => {
     .filter((chocolate) => chocolate.brandId === brandId);
 };
 
+const getTotalChocolates = async () => {
+  const cacaoTrybe = await readCacaoTrybeFile();
+  return cacaoTrybe.chocolates.length;
+};
+
+const getChocolateByName = async (name) => {
+  const cacaoTrybe = await readCacaoTrybeFile();
+  const { chocolates } = cacaoTrybe;
+  return chocolates.filter(({ name: chocolateName }) => chocolateName.includes(name));
+};
+
+const updateChocolateById = async (id, name, brandId) => {
+  const cacaoTrybe = await readCacaoTrybeFile();
+  const chocolatesUpdated = cacaoTrybe.chocolates.map((chocolate) => {
+    if (chocolate.id === Number(id)) {
+      chocolate.name = name;
+      chocolate.brandId = brandId;
+    }
+    return chocolate;
+  });
+  cacaoTrybe.chocolate = { ...chocolatesUpdated };
+  await writeCacaoTrybeFile(cacaoTrybe);
+  return chocolatesUpdated.find(({ id: chocoId }) => chocoId === Number(id));
+}
+
 module.exports = {
   getAllChocolates,
   getChocolateById,
   getChocolatesByBrand,
+  getTotalChocolates,
+  getChocolateByName,
+  updateChocolateById,
 };
--- Back-end/Bloco-22-introducao-ao-nodeJS/dia-3-Testes-de-integracao/exercicios-fixacao/cacao-trybe/tests/integration/chocolates.test.js
+++ Back-end/Bloco-22-introducao-ao-nodeJS/dia-3-Testes-de-integracao/exercicios-fixacao/cacao-trybe/tests/integration/chocolates.test.js
@@ -110,4 +110,72 @@ describe('Testando a API Cacao Trybe', function () {
       ]);
     });
   });
+
+  describe('Usando médoto GET em /chocolates/total para buscar o total de chocolates', function () {
+    it('Retorna o total de chocolates', async function () {
+      const response = await chai.request(app).get('/chocolates/total');
+
+      expect(response.status).to.be.equal(200);
+      expect(response.body).to.be.deep.equal({
+        "totalChocolates": 4
+      });
+    });
+  });
+
+  describe('Usando médoto GET em /chocolates/search para buscar chocolate pro nome', function () {
+    it('Retorna Mon Chéri e Mound', async function () {
+      const response = await chai.request(app).get('/chocolates/search?name=Mo');
+
+      expect(response.status).to.be.equal(200);
+      expect(response.body.chocolates).to.be.deep.equal([
+        {
+          "id": 3,
+          "name": "Mon Chéri",
+          "brandId": 2
+        },
+        {
+          "id": 4,
+          "name": "Mounds",
+          "brandId": 3
+        }
+      ]);
+    });
+
+    it('Retorna um array vazio e status 404', async function () {
+      const response = await chai.request(app).get('/chocolates/search?name=ZZZ');
+
+      expect(response.status).to.be.equal(404);
+      expect(response.body.chocolates).to.be.deep.equal([]);
+    });
+  });
+
+  describe('Usando médoto PUT em /chocolates/:id para alterar um chocolate', function () {
+    it('Altera o chocolate de id 1 pelo endpoit /chocolates/1', async function () {
+      const response = await chai.request(app).put('/chocolates/1').send({ 
+        "name": "Mint Pretty Good",
+        "brandId": 2
+      });
+
+      expect(response.status).to.be.equal(200);
+      expect(response.body).to.be.deep.equal({
+        "chocolate": { 
+          "id": 1,
+          "name": "Mint Pretty Good",
+          "brandId": 2
+        }
+      });
+    });
+
+    it('Retorna uma mensagem de erro e status 404 para o endpoint /chocolates/555', async function () {
+      const response = await chai.request(app).put('/chocolates/555').send({ 
+        "name": "Mint Pretty Good",
+        "brandId": 2
+      });
+
+      expect(response.status).to.be.equal(404);
+      expect(response.body).to.be.deep.equal({ 
+        "message": "chocolate not found"
+      });
+    });
+  });
 });
