6976a7ccc275177301a1a9bb85b3a418aa53e544
Aron Adams <aronrapetto@hotmail.com>
2022-10-06 04:10:38 -0300

    exercicios fixacao dia 8.2

---
+++ Back-end/secao-8-introducao-a-TypeScript/dia-2-tipagem-estatica-e-generics/exercicios-fixacao/types-unions-aliases-assertions-generics-class-interface/type-aliases.ts
@@ -0,0 +1,15 @@
+type Bird = {
+  color: string,
+  species: string,
+  isInExtinction: boolean,
+  population: number,
+}
+
+type address = {
+  address: string,
+  district: string,
+  city: string,
+  state: string,
+}
+
+type sum = (a:number, b:number) => number;
